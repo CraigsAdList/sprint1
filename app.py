1b6f22b40e55f6d149c3fc18f748ca96eccc1585
Pavel Popov <pavelpopov@Pavels-MacBook-Pro.local>
2022-04-01 17:28:06 -0400

    imported packages, .env, set database, flask-login and some endpoints

--- app.py
+++ app.py
@@ -1,6 +1,34 @@
+"""Module for running flask and setting up endpoints"""
+
+import os
+
 import flask
 
+from flask_login import current_user, login_user, logout_user, LoginManager
+
+from dotenv import load_dotenv, find_dotenv
+
+from models import db, Account, Ad, Channel
+
+load_dotenv(find_dotenv())
+
 app = flask.Flask(__name__)
+app.config["SQLALCHEMY_DATABASE_URI"] = os.getenv("DATABASE_URL")
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.secret_key = os.getenv("SECRET_KEY")
+
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(user_id):
+    """Stolen from some tutorial on flask-login. While it is not explicitly used
+    here, it is required by flask-login"""
+    return Account.query.get(int(id))
 
 # set up a separate route to serve the index.html file generated
 # by create-react-app/npm run build.
@@ -22,7 +50,21 @@ def index():
     # Flask will stop serving this React page correctly
     return flask.render_template("index.html")
 
+@bp.route("/login", methods=["POST"])
+def login():
+    """Handle login"""
+    pass
+
+@bp.route("/signup", methods=["POST"])
+def login():
+    """Handle signup"""
+    pass
+
+@bp.route("/logout", methods=["POST"])
+def login():
+    """Handle logout"""
+    pass
 
 app.register_blueprint(bp)
 
-app.run()
+app.run(debug=True)
