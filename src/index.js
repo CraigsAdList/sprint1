6e9acc79601d0ac64f6285780ef28432bf4bb0ef
Pavel Popov <pavelpopov@Pavels-MacBook-Pro.local>
2022-04-01 23:11:46 -0400

    imported bootstrap

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import 'bootstrap/dist/css/bootstrap.css';
+// Put any other imports below so that CSS from your
+// components takes precedence over default styles.
 import './index.css';
 import App from './App';
 
