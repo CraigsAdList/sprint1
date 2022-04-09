acc4ca1f0b5ae87952defcc4240527aa44e32256
Shawn408 <55267979+Shawn408@users.noreply.github.com>
2022-04-08 22:57:22 -0400

    Update app.py

--- app.py
+++ app.py
@@ -10,9 +10,9 @@ import flask
 
 from flask_login import current_user, login_user, logout_user, LoginManager
 
-from flask import render_template
+from flask import render_template, request
 
-from db_utils import createAd, deleteAllAds, getAdsByOwnerEmail, getAllAccounts, getAllAds
+from db_utils import createAd
 
 from werkzeug.security import generate_password_hash, check_password_hash
 
@@ -29,7 +29,7 @@ app.secret_key = os.getenv("SECRET_KEY")
 
 db.init_app(app)
 with app.app_context():
-    
+
     db.create_all()
 
 login_manager = LoginManager()
@@ -71,7 +71,7 @@ def index():
     return flask.render_template("index.html")
 
 
-@bp.route("/handle_login", methods=["POST"])
+@bp.route("/handle_login", methods=["GET"])
 def handle_login():
     """Handle login"""
     if flask.request.method == "POST":
@@ -101,7 +101,7 @@ def handle_login():
             )
 
 
-@bp.route("/handle_signup", methods=["POST"])
+@bp.route("/handle_signup", methods=["GET"])
 def handle_signup():
     """Handle signup"""
     if flask.request.method == "POST":
@@ -147,9 +147,11 @@ def handle_logout():
     logout_user()
     return is_logged_in()
 
+
 @app.route("/getaccounts", methods=["GET"])
 def getAccounts():
-    return flask.jsonify({"accounts":getAllAccounts()})
+    return flask.jsonify({"accounts": getAllAccounts()})
+
 
 @bp.route("/is_logged_in", methods=["GET"])
 def is_logged_in():
@@ -159,32 +161,10 @@ def is_logged_in():
         return flask.jsonify({"isuserloggedin": False})
 
 
-@bp.route("/account_info", methods=["GET", "POST"])
+@bp.route("/account_info", methods=["GET"])
 def account_info():
     """Return current user's JSON data"""
-    current_account = current_user.username
-    account = Account.query.filter_by(username=current_account).first()
-    adLog = Ad.query.filter_by(account_id=account.id).all()
-    channelLog = Channel.query.filter_by(account_id=account.id).all()
-    adList = []
-    for i in adLog:
-        adDict = {}
-        adDict["title"] = i.title
-        adDict["topics"] = i.topics
-        adDict["text"] = i.text
-        adDict["reward"] = i.reward
-        adList.append(adDict)
-    channelList = []
-    for i in channelLog:
-        channelDict = {}
-        channelDict["channel_name"] = i.channel_name
-        channelDict["subscribers"] = i.subscribers
-        channelDict["topics"] = i.topics
-        channelDict["preferred_reward"] = i.preferred_reward
-        channelList.append(channelDict)
-    return flask.jsonify(
-        {"account": current_account, "ads": adList, "channels": channelList}
-    )
+    pass
 
 
 @bp.route("/return_ads", methods=["GET"])
@@ -231,23 +211,77 @@ def add_channel():
 
 @bp.route("/add_ad", methods=["POST"])
 def add_ad():
-    """Add ad info to database"""
-    pass
+    createAd(
+        flask.request.json["title"],
+        flask.request.json["topics"],
+        flask.request.json["text"],
+        flask.request.json["reward"],
+    )
+    return flask.jsonify({"success": True})
 
 
-@bp.route("/make_response", methods=["POST"])
+@bp.route("/proccess_emails", methods=["GET"])
+def proccess_emails():
+    if request.method == "POST":
+        data = flask.request.form
+        email = data["email"]
+        user = Account.query.filter_by(email=email).first()
+        if user is not None:
+            return flask.jsonify({"success": True})
+        else:
+            return flask.jsonify({"success": False})
+
+
+@bp.route("/make_response", methods=["GET"])
 def make_response():
-    """Handles response"""
-    pass
+    if request.method == "POST":
+        data = flask.request.form
+        response = Response(
+            text=data["text"],
+            ad_id=data["adId"],
+            owner_id=data["ownerId"],
+            channel_id=data["channelId"],
+            title=data["title"],
+            topics=data["topics"],
+            reward=data["reward"],
+            channel_name=data["channel_name"],
+            subscribers=data["subscribers"],
+            preferred_reward=data["preferred_reward"],
+        )
+        if response.preferred_reward > response.reward:
+            response.text = "Sorry, but your preferred reward is higher than the reward you offered. Please try again."
+            return flask.jsonify({"success": False})
+
+        db.session.add(response)
+        db.session.commit()
+        return flask.jsonify({"success": True})
 
 
 @bp.route("/make_offer", methods=["GET"])
 def make_offer():
-    """Handles offer"""
-    pass
+    if request.method == "POST":
+        data = flask.request.form
+        response = Response(
+            text=data["text"],
+            ad_id=data["adId"],
+            owner_id=data["ownerId"],
+            channel_id=data["channelId"],
+            title=data["title"],
+            topics=data["topics"],
+            reward=data["reward"],
+            channel_name=data["channel_name"],
+            subscribers=data["subscribers"],
+            preferred_reward=data["preferred_reward"],
+        )
+        if response.preferred_reward < response.reward:
+            response.text = "Sorry, but your preferred reward is higher than the reward you offered. Please try again."
+            return flask.jsonify({"success": False})
+
+        db.session.add(response)
+        db.session.commit()
+        return flask.jsonify({"success": True})
 
 
 app.register_blueprint(bp)
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.run(debug=True)
