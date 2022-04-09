5643e24c914520a1c56e525b06f84aaf8299b443
Shawn408 <jokershawn@DESKTOP-04IPIDJ>
2022-04-08 20:55:12 -0400

    changed the end of app.py

--- app.py
+++ app.py
@@ -243,4 +243,7 @@ def make_offer():
 
 app.register_blueprint(bp)
 
-app.run(debug=True)
+if __name__ == "__main__":
+    app.run(
+        host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
+    )
