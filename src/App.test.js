fd9aeecda8da2399c4e4efb3ca2bbe80576428aa
Ismael Deka <ismael.deka7899@gmail.com>
2022-04-08 20:26:32 -0400

    fixed App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,7 +1,8 @@
 import { render, screen } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 it('renders welcome message', () => {
-  render(<App />);
+  render(<BrowserRouter><App /></BrowserRouter>);
   expect(screen.getByText('Welcome to the AdsPage!')).toBeInTheDocument();
 });
