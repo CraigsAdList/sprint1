618f742ed3a71aeaf20c10927dd096af6c52f291
Shawn408 <55267979+Shawn408@users.noreply.github.com>
2022-04-08 22:54:57 -0400

    Update app.py

--- app.py
+++ app.py
@@ -249,13 +249,10 @@ def make_offer():
 
 app.register_blueprint(bp)
 
-<<<<<<< HEAD
 if __name__ == "__main__":
     app.run(
         host=os.getenv("IP", "0.0.0.0"), port=int(os.getenv("PORT", 8080)), debug=True
     )
 =======
 if __name__ == '__main__':
-    app.run()
-
->>>>>>> main
+    app.run(debug=True)
